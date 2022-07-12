fa2c556e9331e5148b91ea63176bb38aaa59eafb
Chai <csaetern@alphaworks.tech>
2022-07-12 15:20:46 -0700

    update commit

--- template-parts/projects.php
+++ template-parts/projects.php
@@ -13,21 +13,25 @@
         
         <a href="<?php the_field('project_link_1'); ?>" class="project">
             <img src="<?php the_field('project_bg_1'); ?>" alt="" class="project-bg">
+            <h4 class="project-visit">Visit</h4>
             <h4 class="project-desc"><?php the_field('project_desc_1'); ?></h4>
             <h3 class="project-title"><?php the_field('project_title_1'); ?></h3>
         </a>
         <a href="<?php the_field('project_link_3'); ?>" class="project">
             <img src="<?php the_field('project_bg_3'); ?>" alt="" class="project-bg">
+            <h4 class="project-visit">Visit</h4>
             <h4 class="project-desc"><?php the_field('project_desc_3'); ?></h4>
             <h3 class="project-title"><?php the_field('project_title_3'); ?></h3>
         </a>
         <a href="<?php the_field('project_link_2'); ?>" class="project">
             <img src="<?php the_field('project_bg_2'); ?>" alt="" class="project-bg">
+            <h4 class="project-visit">Visit</h4>
             <h4 class="project-desc"><?php the_field('project_desc_2'); ?></h4>
             <h3 class="project-title"><?php the_field('project_title_2'); ?></h3>
         </a>
         <a href="<?php the_field('project_link_4'); ?>" class="project">
             <img src="<?php the_field('project_bg_4'); ?>" alt="" class="project-bg">
+            <h4 class="project-visit">Visit</h4>
             <h4 class="project-desc"><?php the_field('project_desc_4'); ?></h4>
             <h3 class="project-title"><?php the_field('project_title_4'); ?></h3>
         </a>
--- template-parts/skill-card.php
+++ template-parts/skill-card.php
@@ -16,9 +16,9 @@
         <?php 
         $skills = array(
             array('lang' => 'HTML5', 'effc' => '79%', 'icon' => 'https://cdn-icons-png.flaticon.com/512/1532/1532556.png'),
-            array('lang' => 'CSS', 'effc' => '77%', 'icon' => 'https://cdn-icons-png.flaticon.com/512/186/186319.png'),
+            array('lang' => 'CSS/SASS', 'effc' => '77%', 'icon' => 'https://cdn-icons-png.flaticon.com/512/186/186319.png'),
             array('lang' => 'JS/ES6', 'effc' => '68%', 'icon' => 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png'),
-            array('lang' => 'PHP/WordPress', 'effc' => '44%', 'icon' => 'https://cdn-icons-png.flaticon.com/512/174/174881.png'),
+            array('lang' => 'PHP/WordPress', 'effc' => '53%', 'icon' => 'https://cdn-icons-png.flaticon.com/512/174/174881.png'),
             array('lang' => 'Still Learning', 'effc' => '100%', 'hours' => 'Lifetime', 'icon' => 'https://cdn-icons-png.flaticon.com/512/4616/4616734.png')
         );
         for($i = 0; $i < count($skills); $i++) {
